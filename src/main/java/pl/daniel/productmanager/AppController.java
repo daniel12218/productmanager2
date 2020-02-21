72d474420b332f3597ba6804269198b50b71bc15
Daniel Sacha <daniel181222@onet.pl>
2020-02-21 20:19:24 +0100

    1??

---
+++ src/main/java/pl/daniel/productmanager/AppController.java
@@ -0,0 +1,50 @@
+package pl.daniel.productmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+    private ProductService productService;
+    @Autowired
+    public AppController(ProductService productService) {
+        this.productService = productService;
+    }
+    @RequestMapping("/")
+    public String viewHomePage(Model model){
+        List<Product> listProducts = productService.listAll();
+        model.addAttribute("listProducts",listProducts);
+        return "index";
+    }
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model){
+        Product product = new Product();
+        model.addAttribute("product",product);
+        return "new_product";
+    }
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product){
+        productService.save(product);
+        return "redirect:/";
+    }
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name="id") Long id){
+        ModelAndView modelAndView = new ModelAndView("edit_product");
+        Product product = productService.get(id);
+        modelAndView.addObject("product",product);
+        return modelAndView;
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") Long id){
+        productService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/pl/daniel/productmanager/ProductRepository.java
@@ -0,0 +1,6 @@
+package pl.daniel.productmanager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
