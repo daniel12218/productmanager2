75c95f5913775fe16c3a7843fd5db1a5ccc8a3a4
Daniel Sacha <daniel181222@onet.pl>
2020-02-22 15:25:53 +0100

    zmiana RequestMapping(method = RequestMethod.POST) na PostMapping

--- src/main/java/pl/daniel/productmanager/AppController.java
+++ src/main/java/pl/daniel/productmanager/AppController.java
@@ -3,10 +3,7 @@ package pl.daniel.productmanager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -30,7 +27,7 @@ public class AppController {
         model.addAttribute("product",product);
         return "new_product";
     }
-    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @PostMapping("/save")
     public String saveProduct(@ModelAttribute("product") Product product){
         productService.save(product);
         return "redirect:/";
